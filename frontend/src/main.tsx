611a9e4008229a96f64e07fea103c1245bb6b492
Abhishek Singh <as8998013@gmail.com>
2025-01-20 21:24:25 +0530

    minor fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter , HashRouter} from 'react-router'
 import store from './store/store.ts'
+import { Handshake } from 'lucide-react'
 
 createRoot(document.getElementById('root')!).render(
+ <HashRouter>
 
- <BrowserRouter>
  <Provider store={store} >
     <App/>
  </Provider>
  
- </BrowserRouter>
+ </HashRouter>
 )
